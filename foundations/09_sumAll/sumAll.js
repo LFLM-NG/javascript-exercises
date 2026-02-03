f8a066d85a1fdc9d809ba9b0909d9d604b46ce14
LFLM-NG <gergoryheffley@gmail.com>
2026-02-03 16:30:38 +0700

    Add answer to 09 for foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (Number.isInteger(a) && Number.isInteger(b) 
+        && a >= 0 && b >= 0) {
+            let sum = 0;
+            if (a < b) {
+                for (a; a <= b; a++) sum += a;
+            } else {
+                for (b; b <= a; b++) sum += b;
+            }
+            return sum;
+    } else return `ERROR`; 
 };
 
 // Do not edit below this line
