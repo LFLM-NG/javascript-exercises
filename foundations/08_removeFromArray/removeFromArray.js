4fc7a96e5f3f1ef4d662e6eb5ab483f9c0a58a97
LFLM-NG <gergoryheffley@gmail.com>
2026-02-03 15:52:23 +0700

    Add answer to 08 for foundations
    Algorithm manually removes each argument from array
    
    Did not realize ...args can use array method include(), so could not use
    filter() efficiently

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    for (const item of items) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === item) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
