226baa66bbc698a12d31320d119e6a2cac33cbfc
LFLM-NG <gergoryheffley@gmail.com>
2026-02-16 14:29:13 +0700

    Add answer to 12 for foundations
    Added an extra empty array test case for multiply function

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return (arr.length === 0) ? 0 : arr.reduce((total, num) => total * num);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  while (num > 1) {
+    result *= num;
+    num--;
+  }
+  return result;
 };
 
 // Do not edit below this line
