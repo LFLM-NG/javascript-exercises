130b16fffd8c438c57e1139e2eb8d9e8e368bd99
LFLM-NG <gergoryheffley@gmail.com>
2026-02-16 16:10:55 +0700

    Add answer to 13 for foundations
    Did not use regex since the curriculum hasn't explicitly introduced it

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const strAlphaNumericArr = str
+        .toLowerCase()
+        .split(``)
+        .filter(char => {
+        const strNonAlphaNumeric = ` !,.`;
+            return !strNonAlphaNumeric.includes(char);
+        });
+    const strAlphaNumeric = strAlphaNumericArr.join(``);
+    const strReverse = strAlphaNumericArr.reverse().join(``);
+    return strAlphaNumeric === strReverse;
 };
 
 // Do not edit below this line
