ec8c4b2bb77e95326da69cc57d1e4a7d14cf6f97
LFLM-NG <gergoryheffley@gmail.com>
2026-02-16 19:05:54 +0700

    Add answer to 16 for foundations

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    function age(person) {
+        if (!person.yearOfDeath) return (new Date().getFullYear()) - person.yearOfBirth;
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+    return arr.sort((person1, person2) => age(person2) - age(person1)).at(0);
 };
 
 // Do not edit below this line
